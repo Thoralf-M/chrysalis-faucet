5ec1fd7764bb11a82b8aecc1e5e52039f013dade
Thoralf-M <thoralf.mue@gmail.com>
2021-03-25 16:19:39 +0100

    use iota-client

--- faucet.js
+++ faucet.js
@@ -1,90 +1,71 @@
 require('dotenv').config();
 
 async function run() {
-	const { AccountManager, StorageType, RemainderValueStrategy, initLogger } = require('iota-wallet')
-    const manager = new AccountManager({
-        storagePath: './faucet-database',
-        storageType: StorageType.Sqlite
-    })
+    const { ClientBuilder } = require('@iota/client')
+    const client = new ClientBuilder()
+        .node('https://api.hornet-1.testnet.chrysalis2.com')
+        .disableNodeSync()
+        .localPow(true)
+        .build()
 
-    initLogger({
-      color_enabled: true,
-      outputs: [{
-        name: './faucet.log',
-        level: 'debug'
-      }]
-    })
+    const addresses = await client.getAddresses(process.env.NONSECURE_USE_OF_DEVELOPMENT_SEED_1)
+        .range(0, 1)
+        .get()
+    console.log('Need a refill? Send it this address:', addresses[0])
 
-    manager.setStrongholdPassword(process.env.STRONGHOLD_PASSWORD)
-
-    console.log(manager.getAccounts())
-    const account = manager.getAccount('Faucet pool')
-
-    console.log('alias', account.alias())
-    console.log('syncing...')
-    
-    const synced = await account.sync({})
-
-    //console.log('Account messages: ', account.listMessages())
-    const addresses = account.listAddresses(true)
-    let unspent_addr = '';
-    if(addresses.length == 0) {
-        unspent_addr = account.generateAddress().address
-    } else {
-        unspent_addr = addresses[0].address
-    }
-    console.log('Need a refill? Send it this address:', unspent_addr)
-    
-    const s_addresses = account.listAddresses(false)
-    console.log(s_addresses)
-
+    let balance = await client.getBalance(process.env.NONSECURE_USE_OF_DEVELOPMENT_SEED_1).get();
+    console.log('Balance:', balance)
     // Webserver part
 
     const express = require('express')
     const rateLimit = require('express-rate-limit')
     const app = express()
     const port = process.env.PORT || 3000
-    const secs = parseInt(process.env.RATE_LIMIT_SECONDS || 300)
-    const amount = parseInt(process.env.TOKENS_TO_SEND || 1000)
+    const secs = parseInt(process.env.RATE_LIMIT_SECONDS || 60)
+    const amount = parseInt(process.env.TOKENS_TO_SEND || 1000000)
 
     const limiter = rateLimit({
         windowMs: 1000 * secs,
         max: 1,
         skipFailedRequests: true,
-        message: {'message': `You can only request tokens from the faucet once every ${secs} seconds.`}
+        message: { 'message': `You can only request tokens from the faucet once every ${secs} seconds.` }
     });
-    
+
     app.set('trust proxy', true);
     app.use(express.static('frontend/public'))
     app.use('/api', limiter);
 
     app.get('/api', async (req, res) => {
-        
+
         console.log('API called by ', req.ip, req.ips);
-        if(!req.query.address || req.query.address.length != 64 || req.query.address.indexOf('atoi1') !== 0) {
+        if (!req.query.address || req.query.address.length != 64 || req.query.address.indexOf('atoi1') !== 0 || req.query.address == addresses[0]) {
             res.status(400);
-            res.send({'message': 'Invalid address provided!'})
+            res.send({ 'message': 'Invalid address provided!' })
             return;
         }
-        
+
         try {
-            manager.setStrongholdPassword(process.env.STRONGHOLD_PASSWORD)
-            const node_res = await account.send(
-                req.query.address,
-                amount, 
-                {
-                    remainderValueStrategy: RemainderValueStrategy.reuseAddress(),
-                    indexation: {
-                        index: 'FAUCET'
-                    }
-                }
-            );
-            console.log(node_res);
-            res.send({'message': 'Faucet tokens sent!', 'data': node_res})
+            const inputs = await client.findOutputs([], addresses);
+            console.log("Inputs length: " + inputs.length);
+            let messageBuilder = client
+                .message()
+                .seed(process.env.NONSECURE_USE_OF_DEVELOPMENT_SEED_1);
+            let input_balance = 0
+            for (input of inputs) {
+                input_balance += input.amount;
+                messageBuilder = messageBuilder.input(input.transactionId, input.outputIndex)
+            }
+            if (input_balance - amount != 0 && inputs.length > 1) {
+                messageBuilder = messageBuilder.output(addresses[0], input_balance - amount)
+            }
+            let message = await messageBuilder.output(req.query.address, amount)
+                .submit()
+            console.log(message);
+            res.send({ 'message': 'Faucet tokens sent!', 'data': { id: message.messageId } })
         } catch (e) {
             console.log('ERROR', e);
             res.status(503);
-            res.send({'message': 'Out of service, please try again later', 'error': e})
+            res.send({ 'message': 'Please try again later: \n' + e })
             return;
         }
     });
